95bbb457bc4aec83f0af1509c61952756e06e233
RandomSummer <sksofiqueefiaz2015@gmail.com>
2024-09-07 23:06:34 +0530

    second commit

---
+++ Variables/output.java
@@ -0,0 +1,10 @@
+class output {
+    public static void main(String[] args) {
+//        using println
+        System.out.println("Hello World 1");
+//        using print
+        System.out.print("Hello World 2");
+//        using \n
+        System.out.print("\nHello World 3");
+    }
+}
