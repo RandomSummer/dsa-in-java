b8189a70c4ce23687f6dab0ee62b164db6c090ca
RandomSummer <sksofiqueefiaz2015@gmail.com>
2024-09-08 10:37:53 +0530

    modified question2

--- Variables/Question2.java
+++ Variables/Question2.java
@@ -5,7 +5,10 @@ import java.util.Scanner;
 class Question2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the side of the square : ");
+        int side =  sc.nextInt();
 
-        sc.close;
+        System.out.println("The are of the square is : " + (side*side));
+        sc.close();
     }
 }
