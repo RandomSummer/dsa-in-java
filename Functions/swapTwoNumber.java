4345126b394dff18583eb57e537c3625b1cfbc61
RandomSummer <sksofiqueefiaz2015@gmail.com>
2024-09-10 16:37:52 +0530

    modified file

--- Functions/swapTwoNumber.java
+++ Functions/swapTwoNumber.java
@@ -13,6 +13,7 @@ class swapTwoNumber{
         System.out.println("Before swap :");
         System.out.println("a = " + a + " b = " + b);
         swap(a, b);
+        // Java is call by value
         System.out.println("After swapping (outside function):");
         System.out.println("a = " + a + " b = " + b);
     }
