2fe953be2927567fdfc38f5f55c6ab5b544155f2
RandomSummer <sksofiqueefiaz2015@gmail.com>
2024-09-10 16:16:53 +0530

    default parameter java file

---
+++ Functions/defaultFunc.java
@@ -0,0 +1,10 @@
+class defaultFunc {
+    static public void printHello() {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
+    public static void main(String[] args) {
+        printHello();
+    }
+}
