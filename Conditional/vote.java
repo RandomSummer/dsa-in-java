8bdadbe303678d750179e36503a4c95ccff4669c
RandomSummer <sksofiqueefiaz2015@gmail.com>
2024-09-11 23:25:19 +0530

    modification

--- Conditional/vote.java
+++ Conditional/vote.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class vote {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Enter your age ::");
+        System.out.print("Enter your age :: ");
         int age = scan.nextInt();
         if (age >= 18) {
             System.out.println("You can vote.");
