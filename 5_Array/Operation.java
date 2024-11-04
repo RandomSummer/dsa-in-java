095f7214d1c8ab73e9e9835a6fb2cae43729be63
Sk Sofiquee Fiaz <sksofiqueefiaz2015@gmail.com>
2024-11-04 07:24:17 +0000

    added new file operations

---
+++ 5_Array/Operation.java
@@ -0,0 +1,8 @@
+class Operation {
+    public static void main(String args[]) {
+        int marks[] = {2, 3, 4, 5, 6, 7, 8, 9};
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println(marks[i]);
+        }
+    }
+}
