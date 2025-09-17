4fbffcd4147676c1864572b75c92c10f0cb6a266
Sk Sofiquee Fiaz <sksofiqueefiaz2015@gmail.com>
2025-09-17 04:18:37 +0000

    check if any array is sorted or not

--- 11_Recursion/RecursionBasics.java
+++ 11_Recursion/RecursionBasics.java
@@ -1,5 +1,5 @@
 class RecursionBasics {
-
+    // print nth fibonacci element 
     public static int fib(int n) {
         if (n==0 || n==1){   
             return n;
@@ -12,9 +12,22 @@ class RecursionBasics {
         return fibn;
     }
 
+    // check if array is sorted or not
+    public static boolean isSorted(int[] arr, int i) {
+        if (i == arr.length-1) return true;
+
+        if (arr[i] > arr[i+1]) return false;
+
+        return isSorted(arr, i+1);
+    }
+
     public static void main(String args[]) {
-        int n = 9;
-        System.out.println(fib(n));
+
+        // int n = 9;
+        // System.out.println(fib(n));
+
+        int[] arr = {1, 2, 0, 4, 5};
+        System.out.println(isSorted(arr, 0));
 
     }
 }
