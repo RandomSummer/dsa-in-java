b0d119ee71660be3591fc7ebf2ff36ddaef08b78
Sk Sofiquee Fiaz <sksofiqueefiaz2015@gmail.com>
2025-09-17 03:56:50 +0000

    adding recursion folder

---
+++ 11_Recursion/RecursionBasics.java
@@ -0,0 +1,20 @@
+class RecursionBasics {
+
+    public static int fib(int n) {
+        if (n==0 || n==1){   
+            return n;
+        }
+
+        int fib1 = fib(n-1);
+        int fib2 = fib(n-2);
+        int fibn= fib1 + fib2;
+
+        return fibn;
+    }
+
+    public static void main(String args[]) {
+        int n = 9;
+        System.out.println(fib(n));
+
+    }
+}
